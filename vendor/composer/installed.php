9e12385a63b07187c87b22184b4dc82279ad7fd3
胡刚 <45709122+wekyun@users.noreply.github.com>
2022-02-23 10:09:12 +0800

    Add files via upload

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
+        'pretty_version' => '1.0',
+        'version' => '1.0.0.0',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '9c31d36b320ab011baf8f4f581eb93f8ec2d62a6',
+        'reference' => NULL,
         'name' => 'wekyun/check',
         'dev' => true,
     ),
     'versions' => array(
         'wekyun/check' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
+            'pretty_version' => '1.0',
+            'version' => '1.0.0.0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '9c31d36b320ab011baf8f4f581eb93f8ec2d62a6',
+            'reference' => NULL,
             'dev_requirement' => false,
         ),
     ),
