c531307b616740b47ddf7195715f3041b721fe82
胡刚 <45709122+wekyun@users.noreply.github.com>
2022-02-23 10:39:19 +0800

    Add files via upload

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit461d16c0bed9b07fb93841ed7c118dab
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit461d16c0bed9b07fb93841ed7c118dab', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit461d16c0bed9b07fb93841ed7c118dab', 'loadClassLoader'));
