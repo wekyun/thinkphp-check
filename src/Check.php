9761f2c5d99c3e96383bee64219d25bfbf12ef7d
胡刚 <45709122+wekyun@users.noreply.github.com>
2023-04-13 11:57:47 +0800

    优化和新增不验证指定字段
    优化和新增不验证指定字段；
    在接受的字段前面加一个!即可不验证这个字段是否符合规则，同时优化使用默认字段时不验证是否符合规则。

--- src/Check.php
+++ src/Check.php
@@ -39,7 +39,7 @@ class Check
      * @param string $check_name 场景名
      * @return 调用指定验证场景类的场景
      * */
-    public static function check($name = '', $check_name = '')
+    public static function check2($name = '', $check_name = '')
     {
         $check_key = false;
         $tips_name = false;
@@ -81,11 +81,11 @@ class Check
             if (is_array($check_name)) {
                 foreach ($check_name as $key => $value) {
                     $check_key = null;
-                    if (strpos($value, '>.')) {
+                    if (strpos($value, '>.') !== false) {
                         return self::err_json('验证规则书写错误了 . 必须在 > 前面');
                     }
                     //判断必须
-                    if (strpos($value, '.')) {
+                    if (strpos($value, '.') !== false) {
                         $value_more = explode('.', $value);
                         $check_key_data = explode('|', $value_more[0]);
 
@@ -112,7 +112,7 @@ class Check
                         }
                     }
                     if ($check_key === null) $check_key = $value;
-                    if (strpos($check_key, ':')) {
+                    if (strpos($check_key, ':') !== false) {
                         $check_key_more = explode(':', $check_key);
                         $check_key = $check_key_more[0];
                         if (!self::check_isset_value($param, $check_key)) {
@@ -173,6 +173,160 @@ class Check
         return true;
     }
 
+    public static function check($name = null, $param = null)
+    {
+        $new_data = [];//最终接收的参数
+        //参数为空，接收所有参数
+        $input = input('');
+        if (!$name && !$param) {
+            return input();
+        }
+
+        if (!self::$mapping) {
+            $c = config('');
+            $check = $c['check'];
+            unset($c);
+            if (!$check) return self::err_json('check验证配置为空');
+            if (!isset($check['mapping'])) return self::err_json('验证配置:mapping 未定义');
+            if (count($check['mapping']) == 0) return self::err_json('验证配置:mapping 值为空');
+            self::$mapping = $check['mapping'];
+        }
+
+        $obj = null;
+        if (isset(self::$class[$name])) {
+            $obj = self::$class[$name];
+        } elseif (isset(self::$mapping[$name])) {
+            $class = self::$mapping[$name];
+            try {
+                $obj = new $class();
+                self::$class[$name] = $obj;
+            } catch (\Exception $e) {
+                return self::err_json('验证规则配置文件错误，请检查验证配置文件mapping设置的验证器路径是否正确；' . $e->getMessage());
+            }
+        }
+
+        if (is_string($param)) {
+            $param = explode(',', $param);
+        }
+        if (!is_array($param)) {
+            return self::err_json('接收参数请传递数组或者字符串');
+        }
+        //处理：是不是必传，按照什么验证
+        $check_param = [];//指定要接收的参数
+        foreach ($param as $value) {
+            if (strpos($value, '>.') !== false) {
+                return self::err_json('验证规则书写错误了 . 必须在 > 前面');
+            }
+            $value_more = explode('.', $value);//0是key部分未解析的，1是默认值和提示部分的
+            $check_key = null;
+//            dd($value_more);
+            $vali_data = [
+                'key' => '',
+                'def_val' => '',
+                'tips_name' => '',
+                'tips_msg' => '',
+                'is_check_rule' => true,//是否验证规则
+                'is_mast' => false,
+            ];
+            $is_mast = false;
+            if (count($value_more) == 1) {
+                //不是必传
+                $value_more1_rule_val = explode(':', $value_more[0]);
+                if (count($value_more1_rule_val) == 2) {
+                    $value_more[0] = $value_more1_rule_val[0];
+                    $value_more[1] = $value_more1_rule_val[1];
+                    $vali_data['def_val'] = $value_more1_rule_val[1];
+
+                    if (strpos($value_more[1], '>') !== false) {
+                        $value_more1_def_val = explode('>', $value_more[1]);
+                        $vali_data['def_val'] = $value_more1_def_val[0];
+                        $vali_data['tips_msg'] = $value_more1_def_val[1];
+                    }
+                } else {
+                    //只有自定义提示
+                    if (strpos($value_more1_rule_val[0], '>') !== false) {
+                        $value_more1_tip_val = explode('>', $value_more1_rule_val[0]);
+                        $value_more[0] = $value_more1_tip_val[0];
+                        $vali_data['tips_msg'] = $value_more1_tip_val[1];
+                    }
+                }
+            } else {
+                //必传
+                $is_mast = true;
+                if (strpos($value_more[1], ':') !== false) {
+                    return self::err_json('必填项只能设置错误提示，不能设置默认值');
+                }
+                if (strpos($value_more[1], '>') !== false) {
+                    $value_more1_def_val = explode('>', $value_more[1]);
+                    $vali_data['tips_msg'] = $value_more1_def_val[1];
+                    $vali_data['def_val'] = '';
+                }
+            }
+
+            $vali_data['key'] = $value_more[0];
+
+            if (strpos($vali_data['key'], '|') !== false) {
+                $value_more0_val = explode('|', $vali_data['key']);
+                if (!self::is_mast($value_more0_val[1])) {
+                    return self::err_json('使用|设置的提示名称,不能为空。|后面应该定义提示的名称');
+                }
+                $vali_data['key'] = $value_more0_val[0];
+                $vali_data['tips_name'] = $value_more0_val[1];
+            }
+//            dd($vali_data);
+
+            $data_log_name_more = explode('!', $vali_data['key']);
+            $is_check_rule = false;
+            if (count($data_log_name_more) == 2) {
+                //指定不验证这个参数的格式
+                $vali_data['key'] = $data_log_name_more[1];
+            } else {
+                $is_check_rule = true;
+                $vali_data['key'] = $data_log_name_more[0];
+            }
+
+            $check_key = $vali_data['key'];
+            if (!self::check_isset_value($input, $check_key)) {
+                if ($is_mast) {//必传
+                    if ($vali_data['tips_msg'] != '') {
+                        return self::err_json($vali_data['tips_msg']);
+                    } else {
+                        $tps = $vali_data['tips_name'] ? $vali_data['tips_name'] : $check_key;
+                        return self::err_json($tps . '不可为空');
+                    }
+                } else {
+                    //默认参数和空,不验证参数因为不是传递过来的参数,这个是后端自己定义的,说明是可以用的,比如手机不传就为0,就是可以的,传就必须是格式正确的
+                    $is_check_rule = false;
+                    if ($vali_data['def_val']) {
+                        $new_data[$check_key] = $vali_data['def_val'];
+                    } else {
+                        $new_data[$check_key] = '';
+                    }
+                }
+            } else {
+                $new_data[$check_key] = $input[$check_key];
+            }
+
+            if ($is_check_rule) {
+                if (!$obj->check($new_data)) {
+                    $err_msg = $obj->getError();
+                    if ($vali_data['tips_name']) {
+                        $err_msg = str_replace($check_key, $vali_data['tips_name'], $err_msg);
+                    }
+                    return self::err_json($err_msg);
+                }
+            }
+        }
+        return array_merge($input, $new_data);
+    }
+
+    private static function get_param_data($check_name, $input)
+    {
+//        $data = [];
+//        foreach ($check_name as $key => $value) {
+//        }
+    }
+
     //错误提示
     private static function err_json($msg)
     {
@@ -190,7 +344,7 @@ class Check
      * */
     private static function is_mast($val)
     {
-        if (isset($val) && strlen($val) > 0) {
+        if (strlen($val) > 0) {
             return true;
         }
         return false;
